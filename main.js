272f4f1d48e51aaaf342481b885dfcfda1c24b6b
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-16 16:01:46 +0100

    up

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-// var hambMenuShow = false;
-// $('.header-right .fas.fa-bars').on('click', function() {
-//   if (!hambMenuShow) $('.hamburger-menu').show();
-//   else $('.hamburger-menu').hide();
-// }
-// );
-// $('.fas.fa-times').on('click', function() {
-//   $('.hamburger-menu').hide();
-// }
-// );
 $('.fas.fa-bars').click (
   function(){
     $('.hamburger-menu').show()
